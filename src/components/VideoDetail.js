212f3dc18aed164adfd340cba1eff4786e5f6aab
dmytryy <dmytrash.yura@gmail.com>
2019-09-27 15:06:43 -0700

    added semantic styles

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,13 +4,19 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading..</div>
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe title="video player" src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p> {video.snippet.description}</p>
       </div>
-    </div>);
+    </div >);
 };
 
 export default VideoDetail;
