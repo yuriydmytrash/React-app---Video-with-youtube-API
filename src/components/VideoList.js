007100c345b9dedcb8e831472c68de77d2ad370c
dmytryy <dmytrash.yura@gmail.com>
2019-09-27 13:07:14 -0700

    gitignore update

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
+
 
 const VideoList = ({ videos }) => {
+  videos.map((video) => {
+
+  })
   // props.videos
   return <div>{videos.length}</div>
 };
